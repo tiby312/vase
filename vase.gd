1a804811ca436bf66452dfefdcb2a831d7b5c984
Ken Reed <kenakioreed@gmail.com>
2026-01-27 17:25:36 -0500

    n

--- vase.gd
+++ vase.gd
@@ -1,11 +1,15 @@
 extends RigidBody3D
 
+var initial_position: Vector3
+var initial_transform:Basis;
 var finished : bool = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	initial_position = self.global_position;
 	contact_monitor = true;
 	max_contacts_reported = 2;
+	initial_transform=self.transform.basis;
 	pass # Replace with function body.
 
 
@@ -14,8 +18,11 @@ func _process(delta: float) -> void:
 	if not finished:
 		for node in get_colliding_bodies():
 			if node.is_in_group("floor"):
-				print("Hello!")
-				# todo do something here
-				finished = true;
+				self.global_position=initial_position;
+				
+				self.linear_velocity=Vector3.ZERO;
+				self.angular_velocity=Vector3.ZERO;
+				self.transform.basis=initial_transform;
+				#finished = true;
 				return;
 		
