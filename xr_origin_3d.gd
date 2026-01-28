2988c90a381b74212e96216ad749cee23601d55a
Ken Reed <kenakioreed@gmail.com>
2026-01-28 10:30:40 -0500

    n

---
+++ xr_origin_3d.gd
@@ -0,0 +1,24 @@
+extends XROrigin3D
+
+
+
+var player_height;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Ensure the XROrigin3D has a child node that is an XRCamera3D
+	# The camera node is usually automatically added or you add it manually
+	var camera = find_child("XRCamera3D") # Replace "XRCamera3D" if you named it differently
+	if camera:
+		# Get the camera's position in the parent's local space (the XROrigin3D's space)
+		# The y-coordinate is the height above the origin's floor
+		player_height = camera.transform.origin.y
+		print("Player physical height: ", player_height, " meters")
+	else:
+		print("XRCamera3D node not found!")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
